4847ab7c96deee4748b642ab347a58a86a5fff85
Sir-Zac <53125360+Sir-Zac@users.noreply.github.com>
2021-10-20 20:08:54 +0100

    Add files via upload

---
+++ lua/weapons/police_placer_tool/shared.lua
@@ -0,0 +1,26 @@
+SWEP.PrintName = "Police Prop Placer"
+SWEP.Author = "Sir Zac"
+SWEP.Category = "999Emergency"
+SWEP.Instructions = "Hold Left Click: Place chosen prop \nRight Click: Delete target prop \nT: Cycles to next prop \nShift: Resets rotation \nE/R: Rotates or changes skins of haloed entities"
+
+SWEP.Slot = 0
+SWEP.SlotPos = 4
+
+SWEP.Spawnable = true
+SWEP.AdminSpawnable = false
+
+SWEP.ViewModel = ""
+SWEP.WorldModel = ""
+SWEP.UseHands = false
+SWEP.DrawAmmo = false
+SWEP.DrawCrosshair = false 
+
+SWEP.Primary.ClipSize = -1
+SWEP.Primary.DefaultClip = 0
+SWEP.Primary.Automatic = false
+SWEP.Primary.Ammo = ""
+
+SWEP.Secondary.ClipSize = -1
+SWEP.Secondary.DefaultClip = 0
+SWEP.Secondary.Automatic = false
+SWEP.Secondary.Ammo = ""
